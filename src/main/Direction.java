7e7da1d5a389a7c7268c9c747c6622b4cd645b5d
Kshitij Kumar <kshitijkumar021991@gmail.com>
2023-03-08 19:58:43 +0530

    initial commit

---
+++ src/main/Direction.java
@@ -0,0 +1,14 @@
+package com.mycompany.snakegame;
+
+// direction of snake
+enum Direction { 
+    UP, DOWN, LEFT, RIGHT;
+    
+    public boolean isX() {
+        return this == LEFT || this == RIGHT;
+    }
+    
+    public boolean isY() {
+        return this == UP || this == DOWN;
+    }
+}
